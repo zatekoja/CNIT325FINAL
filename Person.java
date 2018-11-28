8bba4abfb6dd81909d5e63e328f45370dbb29264
zatekoja <45440520+zatekoja@users.noreply.github.com>
2018-11-28 18:34:38 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,91 @@
+package cnit325finalproject;
+
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ateko
+ */
+public class Person implements  java.io.Serializable {
+    private String FirstName; 
+    private String Lastname; 
+    private String Address; 
+    private String Zipcode; 
+    
+    public Person (String name,String idnum,String message)
+    {
+        FirstName = name; 
+        Lastname = idnum;
+         
+    }
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return FirstName;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.FirstName = Name;
+    }
+
+    /**
+     * @return the Idnum
+     */
+    public String getIdnum() {
+        return Lastname;
+    }
+
+    /**
+     * @param Idnum the Idnum to set
+     */
+    public void setIdnum(String Idnum) {
+        this.Lastname = Idnum;
+    }
+
+    /**
+     * @return the Address
+     */
+    public String getAddress() {
+        return Address;
+    }
+
+    /**
+     * @param Address the Address to set
+     */
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    /**
+     * @return the Zipcode
+     */
+    public String getZipcode() {
+        return Zipcode;
+    }
+
+    /**
+     * @param Zipcode the Zipcode to set
+     */
+    public void setZipcode(String Zipcode) {
+        this.Zipcode = Zipcode;
+    }
+
+    /**
+     * @return the Message
+     */
+   
+
+   
+}
