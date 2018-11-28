2082cc6a898ed132b9be0b5981fc6ee7cfdd4fd7
zatekoja <45440520+zatekoja@users.noreply.github.com>
2018-11-28 18:16:22 -0500

    Add files via upload

---
+++ MultithreadedServer.java
@@ -0,0 +1,94 @@
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.net.Socket;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ateko
+ */
+public class MultithreadedServer extends Thread implements java.io.Serializable {
+
+    Socket incoming = null;
+    ObjectOutputStream outToClient = null;
+    ObjectInputStream inFromClient = null;
+    Person person_stream1;
+    String A = "";
+    String ICM_Client = "";
+    int size;
+    int count = 0;
+    Server server = null;
+
+    public MultithreadedServer(Socket s, Person p, ObjectInputStream in, ObjectOutputStream out, int c, Server server) {
+        this.incoming = s;
+        this.person_stream1 = p;
+        this.inFromClient = in;
+        this.outToClient = out;
+        this.count = c;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("cleint name:    " + person_stream1.getName());
+        System.out.println("Client socket:    " + incoming.toString());
+        System.out.println("----------------------------");
+        boolean start = true;
+        while (start == true) {
+            try {
+                System.out.println("Waiting for value of A");
+                A = (String) (inFromClient.readObject());
+                System.out.println("------------------Read a value of a = ----------" + A);
+                if (A.equalsIgnoreCase("Send")) {
+
+                    System.out.println("-----READING INCOMING MESSAGE FROM CLIENT------");
+                    ICM_Client = (String) (inFromClient.readObject());
+                    System.out.println("------Received message-----");
+
+                    server.send_message_client(ICM_Client);
+                    System.out.println("------Message written to client-");
+
+
+                    System.out.println("Client name " + person_stream1.getName() + "\n" + "     Incoming message     " + ICM_Client + "\n");
+                    System.out.println("-----------");
+
+                } else {
+                    System.out.println("-------------dkfbsdkj;n;sjdkn;jkdsng;kjdsgn;ds---------------");
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                start = false;
+            } finally {
+                System.out.println("-------------fianlly---------------");
+                return_message();
+            }
+
+        }
+    }
+
+    public String return_message() {
+        String message;
+        message = this.ICM_Client;
+
+        System.out.println(message);
+        return message;
+
+    }
+
+    public void send_message(String msg) {
+        try {
+            outToClient.writeObject(msg);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,109 @@
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ateko
+ */
+public class Server implements  java.io.Serializable {
+       Person person_stream1 = new Person ("end","","end");   
+       ArrayList<Person> Person_list = new ArrayList<>();
+       ArrayList< MultithreadedServer> mts_list = new ArrayList<>();
+       int count ;
+       String ICM_Client = "";       
+       ObjectInputStream inFromClient = null;
+       ObjectOutputStream outToClient = null;
+       MultithreadedServer MtS = null; 
+       
+    public Server(int port){  
+ 
+    try
+        {
+            
+            ServerSocket s = new ServerSocket(port);             
+            System.out.println("Server started");
+            System.out.println("TCPServer Waiting for client on port 8189");
+            
+            while (true)
+            {                
+            Socket incoming = s.accept();  
+            inFromClient = new ObjectInputStream(incoming.getInputStream());
+            outToClient = new ObjectOutputStream(incoming.getOutputStream());            
+            System.out.println("Client connected");
+            count++;               
+            System.out.println("count :" + count); 
+            System.out.println("Reading from client");            
+            String Send = (String)(inFromClient.readObject());
+            System.out.println("String received, string is  " + Send);        
+             System.out.println("Person Read");
+             person_stream1 = (Person)(inFromClient.readObject()); 
+             Person_list.add(count-1, person_stream1);           
+          
+            System.out.println("Array list size :    "+Person_list.size());
+            for (int i = 0; i < Person_list.size(); i++)
+            {
+                System.out.println("Person Name " + Person_list.get(i).getName());
+            }
+           
+            System.out.println("End list");
+            System.out.println("-----------sending count to user-----------------");   
+           
+            System.out.println("----------------count sent------------");   
+            MtS = new MultithreadedServer(incoming,person_stream1,inFromClient,outToClient,count, this);
+            mts_list.add(MtS);
+            System.out.println("----------------------------");
+            System.out.println("Multi threaded server started");
+            MtS.start();
+            this.send_message_client("There are currently "+count + " people in the chatroom");
+            System.out.println("----------------------------");
+            if (MtS.isAlive())
+            {
+            
+            }
+            else
+            { count = 0;}
+            
+
+          
+    }
+        }
+    catch (Exception e)
+    {
+        
+        e.printStackTrace();
+        System.out.println("Connection Error");
+    }
+        
+    
+}
+    public static void main(String[] args)
+    {
+        Server server = new Server(8189); 
+
+}
+    public void send_message_client(String msg)
+    {
+    for (MultithreadedServer m : mts_list)
+    {
+        m.send_message(msg);
+    }
+    }
+
+}
+
