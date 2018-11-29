7f828755967c087efa7658b7d630e803688e241f
sbegman <38700305+sbegman@users.noreply.github.com>
2018-11-28 19:29:50 -0500

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orderconfirmation;
+
+import java.util.*;
+import java.text.*;
+
+/**
+ *
+ * @author Stephen
+ */
+public class Time 
+{
+    public String getTime() 
+    {
+        Date currentTime = new Date();
+        DateFormat formatter = DateFormat.getInstance();
+        String dateStr = formatter.format(currentTime);
+        return dateStr;
+    }
+    public void setTime()
+    {
+         Date currentTime = new Date();
+    }
+}
