94af287e191596ea1cb553051acc81ca46aeecbb
sbegman <38700305+sbegman@users.noreply.github.com>
2018-11-28 19:55:56 -0500

    Update OrderConfirmation.java

--- OrderConfirmation.java
+++ OrderConfirmation.java
@@ -13,84 +13,20 @@ import java.util.Properties;
  *
  * @author Stephen
  */
-public class OrderConfirmation 
+public class OrderConfirmation extends Customer
 {
-    String fName, mName, orderNumber, address1, address2, city, state, zipcode,country, price;
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
+    private String ConfirmationID;
+    private Customer c;
+    
+    public OrderConfirmation(String customerInfo, String fName, String lName, String address, String zipcode)
     {
-        
-    }
-    
-    public OrderConfirmation(String customerInfo)
-    {
-        try
-        {
-            Properties props = new Properties();
-            FileInputStream custFileIn = new FileInputStream(customerInfo);
-            props.load(custFileIn);
+        this.ConfirmationID = ConfirmationID;
+        fName = c.getFirstName();
+        lName = c.getLastName();
+        address = c.getAddress();
+        zipcode = c.getZipcode();
             
-            fName = props.getProperty("update once known");
-            mName = props.getProperty("update once known");
-            orderNumber = props.getProperty("update once known");
-            address1 = props.getProperty("update once known");
-            address2 = props.getProperty("update once known");
-            city = props.getProperty("update once known");
-            state = props.getProperty("update once known");
-            country = props.getProperty("update once known");
-            zipcode = props.getProperty("update once known");
-            price = props.getProperty("update once known");
-            
-        }
-        
-        catch(IOException exc)
-        {
-            exc.printStackTrace();
-        }
     }
-    
-        public String getFName()
-        {
-            return fName;
-        }
-        public String getMName()
-        {
-            return mName;
-        }
-        public String getOrderNum()
-        {
-            return orderNumber;
-        }
-        public String getAddress1()
-        {
-            return address1;
-        }
-        
-        public String getAddress2()
-        {
-            return address2;
-        }
-        public String getCity()
-        {
-            return city;
-        }
-        public String getState()
-        {
-            return state;
-        }
-        public String getCountry()
-        {
-            return country;
-        }
-        public String setZipCode()
-        {
-            return zipcode;
-        }
-        public String getPrice()
-        {
-            return price;
-        }
-    
 }
+    
+  
