3ea5719ea5a17c66a3e57403dc4a7b55fb51e51b
hallcc <hallcc@purdue.edu>
2018-12-02 23:36:44 -0500

    Updated Delivery.java
    Made Delivery an object that extends Order Confirmation. There is a set value in the constructor for estimatedArrival, to return "Your package arrives in 4 business days." The getters and setters for estimatedArrival are there.

--- Delivery.java
+++ Delivery.java
@@ -9,17 +9,34 @@ package cnit325finalproject;
  * TEMPORARY CLASS - Takes input Address To and Address From
  * Returns a number of business days for estimated delivery
  */
-public class Delivery {
+public class Delivery extends OrderConfirmation {
 
-   //Variables are To and From Addresses
+   //Variables are To and From Addresses, and estimatedArrival that is "Determined" by OrderConfirmation information given
     
     private String AddressTo;
     private String AddressFrom;
+    private String estimatedArrival;
 
-    public String EstimatedArrival(String AddressTo, String AddressFrom) {
+    //
+
+    public Delivery(String CustomerID, String name, String address, 
+            String phone_num, String city, String state, String Country, String email, String zipcode) {
+        super(CustomerID, name, address, phone_num, city, state, Country, email, zipcode);
+        this.estimatedArrival = "Your package will arrive in approximately 4 business days.";
+    }
+    
+    public String getEstimatedArrival() {
+        return estimatedArrival;
+    }
+
+    public void setEstimatedArrival(String estimatedArrival) {
+        this.estimatedArrival = estimatedArrival;
+    }
+   
+    /*public String EstimatedArrival(String AddressTo, String AddressFrom) {
         //EstimatedArrival 
         String result = "Ship To: " + AddressTo 
                 + "Your package will arrive in approximately 4 business days.";
         return result;
-    }
+    }*/
 }
