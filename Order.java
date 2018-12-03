83b2dbb27c3b15c925890ecc6d6bf0e269a4d1c7
sbegman <38700305+sbegman@users.noreply.github.com>
2018-12-03 00:14:08 -0500

    Add files via upload

--- Order.java
+++ Order.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package finalproj;
+package cnit325finalproject;
 
 import java.awt.List;
 import java.util.*;
@@ -12,41 +12,54 @@ import java.util.*;
  *
  * @author Stephen
  */
-public class Order extends Customer implements ListofItems
+public class Order 
 {
-    private String custID,fullname,custaddress,phone_num, custemail, custcity, custstate, custcountry, custzipcode;
-    private double price, tax, total;
-    //this class needs to calculate price, 
-    private Customer c = new Customer();
-    public Order(String Customerid,String name, String address, String city, String state, String Country,
-            String zipcode,String Phone,String email, int orderprice, int ordertax, int ordertotal)
-    {
-        custID = Customerid;
-        fullname = name;
-        custaddress = address;
-        phone_num = Phone;
-        custemail = email;
-        custcity = city;
-        custstate = state;
-        custcountry = Country;
-        custzipcode = zipcode;
-        price = orderprice;
-        tax = ordertax;
-        total = ordertotal; 
+
+    /**
+     * @return the supplier
+     */
+    public Supplier getSupplier() {
+        return supplier;
     }
-    public Order()
-    {
-        custID = c.getCustomerID();
-        fullname = c.getName();
-        custaddress = c.getAddress();
-        phone_num = c.getPhone_num();
-        custcity = c.getCity();
-        custstate = c.getState();
-        custcountry = c.getCountry();
-        custemail = c.getEmail();
-        custzipcode = c.getZipcode();
-        price = 500;
-        tax = 1.07;
-        total = price * tax;
+
+    /**
+     * @param supplier the supplier to set
+     */
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
     }
+
+    /**
+     * @return the itemdesc
+     */
+    public String getItemdesc() {
+        return itemdesc;
+    }
+
+    /**
+     * @param itemdesc the itemdesc to set
+     */
+    public void setItemdesc(String itemdesc) {
+        this.itemdesc = itemdesc;
+    }
+
+    /**
+     * @return the OrderID
+     */
+    public String getOrderID() {
+        return OrderID;
+    }
+
+    /**
+     * @param OrderID the OrderID to set
+     */
+    public void setOrderID(String OrderID) {
+        this.OrderID = OrderID;
+    }
+    Customer customer = new Customer("1","Max", "598 Carrol St, West Lafayette, IN", "6715559079", "West Lafayette", "Indiana", "USA", "Max@gmail.com", "47906");
+    private Supplier supplier = new Supplier("1", "Black Market Organs", "513 Illegal Way", "2435551498", "New York", "New York", "USA", "organs@bmo.com", "52903");
+    private String itemdesc = "This item is a heart from Marylin Monroe";
+    private String OrderID = "500";
+    
+    
 }
