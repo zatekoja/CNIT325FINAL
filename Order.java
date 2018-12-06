badee51cd002990a8dc6c55fce32b960ab327dc7
sbegman <38700305+sbegman@users.noreply.github.com>
2018-12-05 20:54:43 -0500

    Update Order.java
    changed hard coded user and supplier data to make it flexible

--- Order.java
+++ Order.java
@@ -56,10 +56,10 @@ public class Order
     public void setOrderID(String OrderID) {
         this.OrderID = OrderID;
     }
-    Customer customer = new Customer("1","Max", "598 Carrol St", "6715559079", "West Lafayette", "Indiana", "USA", "Max@gmail.com", "47906");
-    private Supplier supplier = new Supplier("1", "Black Market Organs", "513 Illegal Way", "2435551498", "New York", "New York", "USA", "organs@bmo.com", "52903");
-    private String itemdesc = "This item is a heart from Marylin Monroe";
-    private String OrderID = "500";
+    Customer customer = new Customer();
+    private Supplier supplier = new Supplier();
+    private String itemdesc = "This item is a heart from Marylin Monroe"; //where is location of this?
+    private String OrderID = "500"; //where is location of this?
     
     
 }
